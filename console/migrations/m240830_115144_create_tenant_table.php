623829b8267f9516f6e4811d75695901ee6f0f0a
mwanginjamb <fnjambi@outlook.com>
2024-08-30 14:56:47 +0300

    Feat: completed the first domain specific migration

---
+++ console/migrations/m240830_115144_create_tenant_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tenant}}`.
+ */
+class m240830_115144_create_tenant_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%tenant}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer(),
+            'principle_tenant_name' => $this->string(),
+            'house_number' => $this->string(),
+            'cell_number' => $this->string(15),
+            'billing_email_address' => $this->string(50),
+            'id_number' => $this->string(50),
+            'agreed_rent_payable' => $this->integer(),
+            'has_signed_tenancy_agreement' => $this->boolean(),
+            'created_at' => $this->integer(),
+            'update_at' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%tenant}}');
+    }
+}
