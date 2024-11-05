5dec4926dc550082b958043834c40e61f433b47b
mwanginjamb <fnjambi@outlook.com>
2024-11-05 12:21:57 +0300

    fix: rence payperiod instance as an array

--- frontend/views/payperiod/pperiods.php
+++ frontend/views/payperiod/pperiods.php
@@ -37,8 +37,8 @@ $this->params['breadcrumbs'][] = $this->title;
                             <td><?= $item->body ?></td>
                             <td><?= $item->payperiodstatus->name ?></td>
                             <td>
-                                <?= Html::a('Update', ['update', 'id' => $item->id], ['_target' => '_blank', 'class' => 'btn btn-warning']) ?>
-                                <?= Html::a('view', ['view', 'id' => $item->id], ['target' => '_blank', , 'class' => 'btn btn-info']) ?>
+                                <?= Html::a('Update', ['update', 'id' => $item['id']], ['_target' => '_blank', 'class' => 'btn btn-warning']) ?>
+                                <?= Html::a('view', ['view', 'id' => $item['id']], ['target' => '_blank', , 'class' => 'btn btn-info']) ?>
                             </td>
                         </tr>
                     <?php endforeach;
