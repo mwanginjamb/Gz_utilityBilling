f1a44a1ba379e039fcf4b3f463f0c9b6b5c68750
mwanginjamb <fnjambi@outlook.com>
2024-11-10 13:08:01 +0300

    add smtp support configs for mailing purposes

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'supportEmail' => env('SMTP_USERNAME'),
+    'senderEmail' => env('SMTP_USERNAME'),
+    'senderName' => env('APP_NAME') . ' Mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
     'generalTitle' => env('APP_NAME'),
