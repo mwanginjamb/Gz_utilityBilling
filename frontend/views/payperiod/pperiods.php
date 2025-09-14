b1ad1c0a9c0bc1997ce9ccc7bcb0ff1b25721dba
mwanginjamb <fnjambi@outlook.com>
2025-09-14 12:35:50 +0300

    Fix: correct reference to payperiod status id

--- frontend/views/payperiod/pperiods.php
+++ frontend/views/payperiod/pperiods.php
@@ -43,8 +43,8 @@ $this->params['breadcrumbs'][] = $this->title;
                             <td><?= $item->body ?></td>
                             <td><?= $item->payperiodstatus->name ?></td>
                             <td>
-                                <?= ($item->payperiodstatus == Payperiod::STATUS_OPEN) ? Html::a('Update', ['update', 'id' => $item->id], ['_target' => '_blank', 'class' => 'btn btn-warning']) : '' ?>
-                                <?= ($item->payperiodstatus == Payperiod::STATUS_CLOSED) ? Html::a('<i class="fas fa-redo mx-1"></i>Reopen', ['reopen'], [
+                                <?= ($item->payperiodstatus_id == Payperiod::STATUS_OPEN) ? Html::a('Update', ['update', 'id' => $item->id], ['_target' => '_blank', 'class' => 'btn btn-warning']) : '' ?>
+                                <?= ($item->payperiodstatus_id == Payperiod::STATUS_CLOSED) ? Html::a('<i class="fas fa-redo mx-1"></i>Reopen', ['reopen'], [
                                     'class' => 'btn btn-success',
                                     'data' => [
                                         'confirm' => Yii::t('app', 'Are you sure you want to reopen this pay period ?'),
